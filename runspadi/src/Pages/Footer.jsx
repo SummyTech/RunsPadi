78cc850b4b18651181ca2fd37e748c2c186681fc
Danjuma1 <hda0202@gmail.com>
2025-09-20 19:56:21 +0100

    added address

--- runspadi/src/Pages/Footer.jsx
+++ runspadi/src/Pages/Footer.jsx
@@ -91,6 +91,12 @@ function Footer(){
                                 <div className="flex flex-col sm:flex-row items-center gap-6 text-gray-300">
                                     <div className="flex items-center gap-2">
                                         <svg className="w-5 h-5 text-red-400" fill="currentColor" viewBox="0 0 20 20">
+                                            <path fillRule="evenodd" d="M5.05 4.05a7 7 0 119.9 9.9L10 18.9l-4.95-4.95a7 7 0 010-9.9zM10 11a2 2 0 100-4 2 2 0 000 4z" clipRule="evenodd" />
+                                        </svg>
+                                        <span className="text-sm">51 Shogbamu Street, New Garage, Gbagada Lagos</span>
+                                    </div>
+                                    <div className="flex items-center gap-2">
+                                        <svg className="w-5 h-5 text-red-400" fill="currentColor" viewBox="0 0 20 20">
                                             <path d="M2 3a1 1 0 011-1h2.153a1 1 0 01.986.836l.74 4.435a1 1 0 01-.54 1.06l-1.548.773a11.037 11.037 0 006.105 6.105l.774-1.548a1 1 0 011.059-.54l4.435.74a1 1 0 01.836.986V17a1 1 0 01-1 1h-2C7.82 18 2 12.18 2 5V3z"/>
                                         </svg>
                                         <span className="text-sm">24/7 Support: 1-800-RUNSPADI</span>
